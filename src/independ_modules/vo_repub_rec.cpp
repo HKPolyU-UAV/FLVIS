2d604ed0c8f4051d925554934097eb2ca2232643
Shengyang Chen <chensy1991@gmail.com>
2020-05-01 13:53:34 +0800

    republish and record
    republish flvis output and record as tum formate.

--- src/independ_modules/vo_repub_rec.cpp
+++ src/independ_modules/vo_repub_rec.cpp
@@ -2,11 +2,14 @@
 #include "std_msgs/String.h"
 #include "geometry_msgs/TransformStamped.h"
 #include "geometry_msgs/PointStamped.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "nav_msgs/Path.h"
 #include "stdio.h"
 #include <fstream>
 
 using namespace  std;
 
+ros::Publisher repub;
 ros::Subscriber sub;
 bool enable_output_file;
 bool enable_repub;
@@ -16,60 +19,98 @@ std::string sub_topic;
 std::string sub_type;
 std::string repub_topic;
 std::string repub_type;
+nav_msgs::Path path;
+int frame_id_path = 0;
 
 static void republishtopic(ros::Time stamp, double x, double y, double z,
                            double qw, double qx, double qy, double qz)
 {
+  if(repub_type == "PoseStamped")
+  {
+    geometry_msgs::PoseStamped latest_pose;
+    latest_pose.header.frame_id = "map";
+    latest_pose.header.stamp=stamp;
+    latest_pose.pose.orientation.w = qw;
+    latest_pose.pose.orientation.x = qx;
+    latest_pose.pose.orientation.y = qy;
+    latest_pose.pose.orientation.z = qz;
+    latest_pose.pose.position.x = x;
+    latest_pose.pose.position.y = y;
+    latest_pose.pose.position.z = z;
+    repub.publish(latest_pose);
+  }
+  if(repub_type == "Path")
+  {
+    geometry_msgs::PoseStamped latest_pose;
+    latest_pose.header.frame_id = "map";
+    latest_pose.header.stamp=stamp;
+    latest_pose.pose.orientation.w = qw;
+    latest_pose.pose.orientation.x = qx;
+    latest_pose.pose.orientation.y = qy;
+    latest_pose.pose.orientation.z = qz;
+    latest_pose.pose.position.x = x;
+    latest_pose.pose.position.y = y;
+    latest_pose.pose.position.z = z;
 
+    path.header.frame_id = "map";
+    path.header.stamp = stamp;
+    path.poses.push_back(latest_pose);
+//    if(path.poses.size()>=1000)
+//    {
+//      path.poses.erase(path.poses.begin());
+//    }
+    repub.publish(path);
+  }
 }
 
 
 static void process(ros::Time stamp, double x, double y, double z,
                double qw, double qx, double qy, double qz)
 {
-  if(enable_output_file)
+  static ros::Time last_time=ros::Time::now();
+  if((ros::Time::now().toSec()-last_time.toSec())>0.1)
   {
-    fd << setprecision(6)
-       << stamp << " "
-       << setprecision(9)
-       << x << " "
-       << y << " "
-       << z << " "
-       << qw << " "
-       << qx << " "
-       << qy << " "
-       << qz << std::endl;
-  }
-  if(enable_repub)
-  {
-    republishtopic(stamp,x,y,z,qw,qx,qy,qz);
+    if(enable_output_file)
+    {
+      fd << setprecision(6)
+         << stamp << " "
+         << setprecision(9)
+         << x << " "
+         << y << " "
+         << z << " "
+         << qw << " "
+         << qx << " "
+         << qy << " "
+         << qz << std::endl;
+    }
+    if(enable_repub)
+    {
+      republishtopic(stamp,x,y,z,qw,qx,qy,qz);
+    }
   }
 }
-
-
-
-
 
 void TransformStamped_callback(const geometry_msgs::TransformStampedConstPtr msg)
 {
   process(msg->header.stamp,
-          msg->transform.translation.x,
-          msg->transform.translation.x,
-          msg->transform.translation.x,
-          msg->transform.rotation.w,
-          msg->transform.rotation.x,
-          msg->transform.rotation.y,
-          msg->transform.rotation.z);
+          msg->transform.translation.x, msg->transform.translation.y, msg->transform.translation.z,
+          msg->transform.rotation.w, msg->transform.rotation.x, msg->transform.rotation.y,  msg->transform.rotation.z);
 }
 
 void PointStamped_callback(const geometry_msgs::PointStampedConstPtr msg)
 {
   process(msg->header.stamp,
-          msg->point.x,
-          msg->point.y,
-          msg->point.z,
+          msg->point.x, msg->point.y, msg->point.z,
           1, 0, 0, 0);
 }
+
+void PoseStamped_callback(const geometry_msgs::PoseStampedConstPtr msg)
+{
+  process(msg->header.stamp,
+          msg->pose.position.x, msg->pose.position.y, msg->pose.position.z,
+          msg->pose.orientation.w, msg->pose.orientation.x, msg->pose.orientation.y, msg->pose.orientation.z);
+}
+
 
 
 
@@ -99,21 +140,28 @@ int main(int argc, char **argv)
   else
   {
     enable_repub = true;
-    if(repub_type == "0")
+    if(repub_type == "PoseStamped")
     {
-      enable_repub = false;
+      repub = nh.advertise<geometry_msgs::PoseStamped>(repub_topic, 2);
+    }
+    if(repub_type == "Path")
+    {
+       repub = nh.advertise<nav_msgs::Path>(repub_topic, 2);;
     }
   }
 
   if(sub_type=="TransformStamped")
   {
-    sub = nh.subscribe(sub_topic, 10, TransformStamped_callback);
+    sub = nh.subscribe(sub_topic, 2, TransformStamped_callback);
   }
   if(sub_type=="PointStamped")
   {
-    sub = nh.subscribe(sub_topic, 10, PointStamped_callback);
+    sub = nh.subscribe(sub_topic, 2, PointStamped_callback);
   }
-
+  if(sub_type=="PoseStamped")
+  {
+    sub = nh.subscribe(sub_topic, 2, PoseStamped_callback);
+  }
 
   ros::spin();
 
