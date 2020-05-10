06d6d6469cc895e2c923037578183e665468ccd7
Shengyang Chen <chensy1991@gmail.com>
2020-05-10 14:13:02 +0800

    Update keyframe_msg.cpp
    remove descriptor from keyframe msg

--- src/utils/keyframe_msg.cpp
+++ src/utils/keyframe_msg.cpp
@@ -65,23 +65,27 @@ void KeyFrameMsg::pub(CameraFrame& frame, ros::Time stamp)
     kf.lm_descriptor_data.layout.dim.push_back(std_msgs::MultiArrayDimension());
 
     kf.lm_descriptor_data.layout.dim[0].label = "lm_descriptor";
-    kf.lm_descriptor_data.layout.dim[0].size = static_cast<uint32_t>(lm_id.size());
-    kf.lm_descriptor_data.layout.dim[0].stride = static_cast<uint32_t>(32*lm_id.size());
+    kf.lm_descriptor_data.layout.dim[0].size = 0;
+    kf.lm_descriptor_data.layout.dim[0].stride = 0;
+//    kf.lm_descriptor_data.layout.dim[0].size = static_cast<uint32_t>(lm_id.size());
+//    kf.lm_descriptor_data.layout.dim[0].stride = static_cast<uint32_t>(32*lm_id.size());
     kf.lm_descriptor_data.layout.dim[1].label = "32uint_descriptor";
-    kf.lm_descriptor_data.layout.dim[1].size = 1;
-    kf.lm_descriptor_data.layout.dim[1].stride = 32;
+    kf.lm_descriptor_data.layout.dim[1].size = 0;
+    kf.lm_descriptor_data.layout.dim[1].stride = 0;
+//    kf.lm_descriptor_data.layout.dim[1].size = 1;
+//    kf.lm_descriptor_data.layout.dim[1].stride = 32;
 
 
-    for(size_t i=0; i<lm_id.size(); i++)
-    {
-        //cout << "i:" << lm_descriptors.at(i).type() << "  " << "size:" << lm_descriptors.at(i).size << endl;
-        //cout << "i " << lm_descriptors.at(i) << endl;
-        for(int j=0; j<32; j++)
-        {
-            kf.lm_descriptor_data.data.push_back(lm_descriptors.at(i).at<uint8_t>(0,j));
-            //cout << unsigned(lm_descriptors.at(i).at<uint8_t>(0,j)) << " ";
-        }
-    }
+//    for(size_t i=0; i<lm_id.size(); i++)
+//    {
+//        //cout << "i:" << lm_descriptors.at(i).type() << "  " << "size:" << lm_descriptors.at(i).size << endl;
+//        //cout << "i " << lm_descriptors.at(i) << endl;
+//        for(int j=0; j<32; j++)
+//        {
+//            kf.lm_descriptor_data.data.push_back(lm_descriptors.at(i).at<uint8_t>(0,j));
+//            //cout << unsigned(lm_descriptors.at(i).at<uint8_t>(0,j)) << " ";
+//        }
+//    }
 
     for(size_t i=0; i<lm_id.size(); i++)
     {
@@ -114,13 +118,13 @@ void KeyFrameMsg::pub(CameraFrame& frame, ros::Time stamp)
 
 void KeyFrameMsg::unpack(flvis::KeyFrameConstPtr kf_const_ptr,
                          int64_t         &frame_id,
-                         cv::Mat             &img,
-                         cv::Mat             &d_img,
+                         cv::Mat         &img,
+                         cv::Mat         &d_img,
                          int             &lm_count,
                          vector<int64_t> &lm_id,
                          vector<Vec2>    &lm_2d,
                          vector<Vec3>    &lm_3d,
-                         vector<cv::Mat>     &lm_descriptors,
+                         vector<cv::Mat> &lm_descriptors,
                          SE3             &T_c_w,
                          ros::Time       &T)
 {
@@ -139,12 +143,12 @@ void KeyFrameMsg::unpack(flvis::KeyFrameConstPtr kf_const_ptr,
     int count =  kf_const_ptr->lm_count;
     for(auto i=0; i<count; i++)
     {
-        cv::Mat descriptor = cv::Mat(1,32,CV_8U);
-        for(auto j=0; j<32; j++)
-        {
-            descriptor.at<uint8_t>(0,j)=kf_const_ptr->lm_descriptor_data.data.at(i*32+j);
-        }
-        lm_descriptors.push_back(descriptor);
+//        cv::Mat descriptor = cv::Mat(1,32,CV_8U);
+//        for(auto j=0; j<32; j++)
+//        {
+//            descriptor.at<uint8_t>(0,j)=kf_const_ptr->lm_descriptor_data.data.at(i*32+j);
+//        }
+//        lm_descriptors.push_back(descriptor);
         //cout << lm_descriptors.at(i) << endl;
         lm_id.push_back(kf_const_ptr->lm_id_data.data[i]);
         Vec2 p2d(kf_const_ptr->lm_2d_data.at(i).x,kf_const_ptr->lm_2d_data.at(i).y);
@@ -154,9 +158,9 @@ void KeyFrameMsg::unpack(flvis::KeyFrameConstPtr kf_const_ptr,
     }
     Vec3 t;
     Quaterniond uq;
-    t[0] = kf_const_ptr->T_c_w.translation.x;
-    t[1] = kf_const_ptr->T_c_w.translation.y;
-    t[2] = kf_const_ptr->T_c_w.translation.z;
+    t(0) = kf_const_ptr->T_c_w.translation.x;
+    t(1) = kf_const_ptr->T_c_w.translation.y;
+    t(2) = kf_const_ptr->T_c_w.translation.z;
     uq.w() = kf_const_ptr->T_c_w.rotation.w;
     uq.x() = kf_const_ptr->T_c_w.rotation.x;
     uq.y() = kf_const_ptr->T_c_w.rotation.y;
